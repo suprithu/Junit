cf53ec178925a27c9bf53eb3f808bae77d7e7ef7
suprith <suprithum@gmail.com>
2024-03-29 11:48:39 +0530

    branch commit

--- src/test/java/com/junit/test/CalcTest.java
+++ src/test/java/com/junit/test/CalcTest.java
@@ -10,8 +10,8 @@ public class CalcTest {
 	public void test()
 	{
 		Calc c = new Calc();
-		int actualRes = c.div(10, 5);
-		int acceptedRes = 2;
+		int actualRes = c.mul(10, 5);
+		int acceptedRes = 50;
 		assertEquals(acceptedRes, actualRes);
 	}
 
